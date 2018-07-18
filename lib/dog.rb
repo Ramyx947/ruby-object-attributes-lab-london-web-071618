e9b2ce7a866af4fa39d871d2e02798feb0929275
Ramona M <r1mari@yahoo.com>
2018-07-18 16:34:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,3 +6,18 @@
 #     @name=new_name
 #   end
 # end
+
+class Dog 
+  
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end 
+    
+    def name
+      @this_dogs_name
+    end
+end 
+  lassie = Dog.new 
+  lassie.name= "Lassie"
+  puts lassie.name
+  
